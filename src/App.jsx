27ad61c9ab0c6faa62a2b25546ec3bbf3f089d29
BriouxMFR <gregory.brioux@mfr.asso.fr>
2025-11-06 16:49:31 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Smoothie Move 🥤</h1>
+      <p>Bienvenue sur ton application de sport et de récompenses !</p>
+    </div>
+  );
+}
