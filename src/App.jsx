99239b722ac82174af5d073c2ceb65095acde1f9
BriouxMFR <gregory.brioux@mfr.asso.fr>
2025-11-07 08:45:26 +0100

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ textAlign: "center", padding: "2rem" }}>
-      <h1>Smoothie Move 🥤</h1>
-      <p>Bienvenue sur ton application de sport et de récompenses !</p>
-    </div>
-  );
-}
